48e0fbcf5587473d3a914bcf4df36e81ae74822b
shin4488 <efshinya48@gmail.com>
2022-11-23 23:13:01 +0900

    change: database user name so that the length regulation is not applied for this user name

--- database/init/1.0.0.sql
+++ database/init/1.0.0.sql
@@ -3,4 +3,4 @@ CREATE DATABASE financial_statement_development;
 \c financial_statement_development;
 ALTER DATABASE financial_statement_development SET timezone TO 'Asia/Tokyo';
 
-GRANT ALL PRIVILEGES ON DATABASE financial_statement_development TO financial_statement_admin_development;
+GRANT ALL PRIVILEGES ON DATABASE financial_statement_development TO financial_statement_admin;
