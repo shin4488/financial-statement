4570fa49ff137052b0c8c7e66779ab859ed5148c
shin4488 <efshinya48@gmail.com>
2023-08-26 18:52:55 +0900

    add: carousel component

--- application/front/src/development/pages/DevCharts.tsx
+++ application/front/src/development/pages/DevCharts.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import AppCarousel from '@/components/appCarousel/AppCarousel';
 import BalanceSheetBarCahrt from '@/components/balanceSheetBarChart/BalanceSheetBarChart';
 import { BalanceSheetBarChartProps } from '@/components/balanceSheetBarChart/props';
 import ProfitLossBarChart from '@/components/profitLossBarChart/ProfitLossBarChart';
@@ -6,10 +7,14 @@ import { ProfitLossBarChartProps } from '@/components/profitLossBarChart/props';
 import CashFlowBarChart from '@/components/cashFlowBarChart/CashFlowBarChart';
 import { CashFlowBarChartProps } from '@/components/cashFlowBarChart/props';
 
+interface FinancialStatement {
+  balanceSheet: BalanceSheetBarChartProps;
+  profitLoss: ProfitLossBarChartProps;
+  cashFlow: CashFlowBarChartProps;
+}
+
 interface DevChartsState {
-  balanceSheetData: BalanceSheetBarChartProps[];
-  profitLossData: ProfitLossBarChartProps[];
-  cashFlowData: CashFlowBarChartProps[];
+  financialStatements: FinancialStatement[];
 }
 
 export default class DevCharts extends React.Component<
@@ -17,66 +22,102 @@ export default class DevCharts extends React.Component<
   DevChartsState
 > {
   state: Readonly<DevChartsState> = {
-    balanceSheetData: [
+    financialStatements: [
       {
-        currentAsset: 250000,
-        propertyPlantAndEquipment: 50000,
-        intangibleAsset: 70000,
-        investmentAndOtherAsset: 10000,
-        currentLiability: 100000,
-        noncurrentLiability: 25000,
-        netAsset: 250000 + 50000 + 70000 + 10000 - (100000 + 25000),
+        balanceSheet: {
+          currentAsset: 250000,
+          propertyPlantAndEquipment: 50000,
+          intangibleAsset: 70000,
+          investmentAndOtherAsset: 10000,
+          currentLiability: 100000,
+          noncurrentLiability: 25000,
+          netAsset: 250000 + 50000 + 70000 + 10000 - (100000 + 25000),
+        },
+        profitLoss: {
+          netSales: 10000,
+          originalCost: 6000,
+          sellingGeneralExpense: 2000,
+          operatingIncome: 10000 - 6000 - 2000,
+        },
+        cashFlow: {
+          startingCash: 400,
+          operatingActivitiesCashFlow: 700,
+          investingActivitiesCashFlow: -300,
+          financingActivitiesCashFlow: 200,
+          endingCash: 400 + 700 + -300 + 200,
+        },
       },
       {
-        currentAsset: 250000,
-        propertyPlantAndEquipment: 50000,
-        intangibleAsset: 70000,
-        investmentAndOtherAsset: 10000,
-        currentLiability: 200000,
-        noncurrentLiability: 350000,
-        netAsset: 250000 + 50000 + 70000 + 10000 - (200000 + 350000),
-      },
-    ],
-    profitLossData: [
-      {
-        netSales: 10000,
-        originalCost: 6000,
-        sellingGeneralExpense: 2000,
-        operatingIncome: 10000 - 6000 - 2000,
-      },
-      {
-        netSales: 10000,
-        originalCost: 6000,
-        sellingGeneralExpense: 5000,
-        operatingIncome: 10000 - 6000 - 5000,
-      },
-      {
-        netSales: 10000,
-        originalCost: 6000,
-        sellingGeneralExpense: 4000,
-        operatingIncome: 10000 - 6000 - 4000,
+        balanceSheet: {
+          currentAsset: 250000,
+          propertyPlantAndEquipment: 50000,
+          intangibleAsset: 70000,
+          investmentAndOtherAsset: 10000,
+          currentLiability: 200000,
+          noncurrentLiability: 350000,
+          netAsset: 250000 + 50000 + 70000 + 10000 - (200000 + 350000),
+        },
+        profitLoss: {
+          netSales: 10000,
+          originalCost: 6000,
+          sellingGeneralExpense: 5000,
+          operatingIncome: 10000 - 6000 - 5000,
+        },
+        cashFlow: {
+          startingCash: 100000,
+          operatingActivitiesCashFlow: -200000,
+          investingActivitiesCashFlow: -300,
+          financingActivitiesCashFlow: 10000,
+          endingCash: 100000 + -200000 + -300 + 10000,
+        },
       },
       {
-        netSales: 10000,
-        originalCost: 6000,
-        sellingGeneralExpense: 3999,
-        operatingIncome: 10000 - 6000 - 3999,
+        balanceSheet: {
+          currentAsset: 250000,
+          propertyPlantAndEquipment: 50000,
+          intangibleAsset: 70000,
+          investmentAndOtherAsset: 10000,
+          currentLiability: 200000,
+          noncurrentLiability: 350000,
+          netAsset: 250000 + 50000 + 70000 + 10000 - (200000 + 350000),
+        },
+        profitLoss: {
+          netSales: 10000,
+          originalCost: 6000,
+          sellingGeneralExpense: 4000,
+          operatingIncome: 10000 - 6000 - 4000,
+        },
+        cashFlow: {
+          startingCash: 100000,
+          operatingActivitiesCashFlow: -200000,
+          investingActivitiesCashFlow: -300,
+          financingActivitiesCashFlow: 10000,
+          endingCash: 100000 + -200000 + -300 + 10000,
+        },
       },
-    ],
-    cashFlowData: [
       {
-        startingCash: 400,
-        operatingActivitiesCashFlow: 700,
-        investingActivitiesCashFlow: -300,
-        financingActivitiesCashFlow: 200,
-        endingCash: 400 + 700 + -300 + 200,
-      },
-      {
-        startingCash: 100000,
-        operatingActivitiesCashFlow: -200000,
-        investingActivitiesCashFlow: -300,
-        financingActivitiesCashFlow: 10000,
-        endingCash: 100000 + -200000 + -300 + 10000,
+        balanceSheet: {
+          currentAsset: 250000,
+          propertyPlantAndEquipment: 50000,
+          intangibleAsset: 70000,
+          investmentAndOtherAsset: 10000,
+          currentLiability: 200000,
+          noncurrentLiability: 350000,
+          netAsset: 250000 + 50000 + 70000 + 10000 - (200000 + 350000),
+        },
+        profitLoss: {
+          netSales: 10000,
+          originalCost: 6000,
+          sellingGeneralExpense: 3999,
+          operatingIncome: 10000 - 6000 - 3999,
+        },
+        cashFlow: {
+          startingCash: 100000,
+          operatingActivitiesCashFlow: -200000,
+          investingActivitiesCashFlow: -300,
+          financingActivitiesCashFlow: 10000,
+          endingCash: 100000 + -200000 + -300 + 10000,
+        },
       },
     ],
   };
@@ -84,37 +125,45 @@ export default class DevCharts extends React.Component<
   render(): React.ReactNode {
     return (
       <>
-        {this.state.balanceSheetData.map((balanceSheet, index) => (
-          <BalanceSheetBarCahrt
-            key={index}
-            currentAsset={balanceSheet.currentAsset}
-            propertyPlantAndEquipment={balanceSheet.propertyPlantAndEquipment}
-            intangibleAsset={balanceSheet.intangibleAsset}
-            investmentAndOtherAsset={balanceSheet.investmentAndOtherAsset}
-            currentLiability={balanceSheet.currentLiability}
-            noncurrentLiability={balanceSheet.noncurrentLiability}
-            netAsset={balanceSheet.netAsset}
-          />
-        ))}
-        {this.state.profitLossData.map((profitLoss, index) => (
-          <ProfitLossBarChart
-            key={index}
-            netSales={profitLoss.netSales}
-            originalCost={profitLoss.originalCost}
-            sellingGeneralExpense={profitLoss.sellingGeneralExpense}
-            operatingIncome={profitLoss.operatingIncome}
-          />
-        ))}
-        {this.state.cashFlowData.map((cashFlow, index) => (
-          <CashFlowBarChart
-            key={index}
-            startingCash={cashFlow.startingCash}
-            operatingActivitiesCashFlow={cashFlow.operatingActivitiesCashFlow}
-            investingActivitiesCashFlow={cashFlow.investingActivitiesCashFlow}
-            financingActivitiesCashFlow={cashFlow.financingActivitiesCashFlow}
-            endingCash={cashFlow.endingCash}
-          />
-        ))}
+        {this.state.financialStatements.map((chartData, index) => {
+          const balanceSheet = chartData.balanceSheet;
+          const profitLoss = chartData.profitLoss;
+          const cashFlow = chartData.cashFlow;
+          return (
+            <AppCarousel key={index}>
+              <BalanceSheetBarCahrt
+                currentAsset={balanceSheet.currentAsset}
+                propertyPlantAndEquipment={
+                  balanceSheet.propertyPlantAndEquipment
+                }
+                intangibleAsset={balanceSheet.intangibleAsset}
+                investmentAndOtherAsset={balanceSheet.investmentAndOtherAsset}
+                currentLiability={balanceSheet.currentLiability}
+                noncurrentLiability={balanceSheet.noncurrentLiability}
+                netAsset={balanceSheet.netAsset}
+              />
+              <ProfitLossBarChart
+                netSales={profitLoss.netSales}
+                originalCost={profitLoss.originalCost}
+                sellingGeneralExpense={profitLoss.sellingGeneralExpense}
+                operatingIncome={profitLoss.operatingIncome}
+              />
+              <CashFlowBarChart
+                startingCash={cashFlow.startingCash}
+                operatingActivitiesCashFlow={
+                  cashFlow.operatingActivitiesCashFlow
+                }
+                investingActivitiesCashFlow={
+                  cashFlow.investingActivitiesCashFlow
+                }
+                financingActivitiesCashFlow={
+                  cashFlow.financingActivitiesCashFlow
+                }
+                endingCash={cashFlow.endingCash}
+              />
+            </AppCarousel>
+          );
+        })}
       </>
     );
   }
