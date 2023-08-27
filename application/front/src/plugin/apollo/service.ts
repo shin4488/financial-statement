7b4a2d5cf7ee3dfe49e3e2e6e384da892e163179
shin4488 <efshinya48@gmail.com>
2023-08-27 21:50:29 +0900

    add: return values & data type of returned values

--- application/front/src/plugin/apollo/service.ts
+++ application/front/src/plugin/apollo/service.ts
@@ -1,3 +1,4 @@
+import { Query } from '@/__generated__/graphql';
 import {
   ApolloClient,
   InMemoryCache,
@@ -25,13 +26,13 @@ export default class ApolloClientService {
       ApolloClientService.client = this.create();
     }
 
-    const result = await ApolloClientService.client.query({
+    const result = await ApolloClientService.client.query<Query>({
       query: gql(literals),
     });
     if (result.error !== undefined) {
       throw Error(result.error.message);
     }
 
-    result.data;
+    return result.data;
   }
 }
