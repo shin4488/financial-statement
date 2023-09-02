c8d5e78ef0764ee7a58425bfe4ee895973063cfd
shin4488 <efshinya48@gmail.com>
2023-09-03 02:52:28 +0900

    change: remove stringutil which was not needed

--- application/front/src/plugins/utils/numberUtil.ts
+++ application/front/src/plugins/utils/numberUtil.ts
@@ -1,10 +1,8 @@
-import StringUtil from '@/plugins/utils/stringUtil';
-
 export class NumberUtil {
   static toNumberOrDefault(
-    value: string | undefined | null,
+    value: string | number | undefined | null,
     defaultValue = 0,
   ): number {
-    return StringUtil.isEmpty(value) ? defaultValue : Number(value);
+    return value === undefined || value === null ? defaultValue : Number(value);
   }
 }
