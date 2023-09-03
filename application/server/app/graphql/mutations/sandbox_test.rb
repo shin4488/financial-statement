bbc8c45bac23083c33bc4a49b9284d2e8518874a
shin4488 <efshinya48@gmail.com>
2023-09-03 18:14:24 +0900

    fix: delete unused sandbox class & move sandbox dirs

--- application/server/app/graphql/mutations/sandbox_test.rb
+++ application/server/app/graphql/mutations/sandbox_test.rb
@@ -1,13 +1,13 @@
 module Mutations
   class SandboxTest < Mutations::BaseMutation
-    argument :attributes, Types::BookmarkType
+    argument :attributes, Types::Sandbox::SandboxBookmarkType
     field :sandbox_testaa, Types::Sandbox::SandboxTestType, null: false
 
     def resolve(attributes:)
       {
         :sandbox_testaa => {
-            :id => attributes[:user_id],
-            :title => attributes[:document_id],
+          :id => attributes[:user_id],
+          :title => attributes[:document_id],
         }
       }
     end
--- application/server/app/graphql/types/bookmark_type.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Types
-  class BookmarkType < Types::BaseInputObject
-    argument :user_id, String, required: true
-    argument :document_id, String, required: true
-  end
-end
