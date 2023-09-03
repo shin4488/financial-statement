814c2986d622ad322e548def3b93f7e6ab499c34
shin4488 <efshinya48@gmail.com>
2023-09-03 21:08:35 +0900

    change: unused controllers

--- application/server/app/controllers/sandbox/sandbox2_controller.rb
+++
@@ -1,9 +0,0 @@
-module Sandbox
-    class Sandbox2Controller < ApplicationController
-        def index
-            render status: :ok, json: {
-                status: 'SUCCESS', message: 'The SAND222!更新！', data: { num: 123, array: ["a", "b"] }
-            }
-        end
-    end
-end
--- application/server/app/controllers/sandbox/sandbox_controller.rb
+++
@@ -1,9 +0,0 @@
-module Sandbox
-    class SandboxController < ApplicationController
-        def index
-            render status: :ok, json: {
-                status: 'SUCCESS', added_data: "追加したデータ", message: 'The SAND!更新！', data: { num: 123, array: ["a", "b"] }
-            }
-        end
-    end
-end
