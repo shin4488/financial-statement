48e5986d8a85f9f97378c3c3b26d63a06ae0a5aa
shin4488 <efshinya48@gmail.com>
2023-09-09 15:42:36 +0900

    add: reference appendix

--- application/front/src/layouts/default/DefaultLayout.tsx
+++ application/front/src/layouts/default/DefaultLayout.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import { DefaultLayoutProps } from './props';
+import { Link } from '@mui/material';
 
 export default class DefaultLayout extends React.Component<DefaultLayoutProps> {
   render(): React.ReactNode {
     // 複数ページ共通で使用したい内容があればこのコンポーネントに記述する
-    return <div>{this.props.children}</div>;
+    return (
+      <>
+        <div>{this.props.children}</div>
+        出典:{' '}
+        <Link
+          target="_blank"
+          href="https://disclosure2dl.edinet-fsa.go.jp/guide/static/disclosure/WZEK0030.html"
+        >
+          EDINET閲覧（提出）サイト
+        </Link>
+        より抜粋して作成
+      </>
+    );
   }
 }
