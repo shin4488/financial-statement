585eb2ba91beae9a342282f00e726bc48a782d3e
shin4488 <efshinya48@gmail.com>
2023-09-13 10:18:49 +0900

    add: log roration

--- application/server/config/application.rb
+++ application/server/config/application.rb
@@ -50,6 +50,7 @@ module FinancialStatement
     # DNSリバインディング攻撃制御に対応するため、nginxで定義されているサーバ名からのリクエストは受け付ける
     config.hosts << ENV["SERVER_HOST_NAME"] if ENV["SERVER_HOST_NAME"].present?
 
+    config.logger = Logger.new("log/#{Rails.env}.log", 'weekly')
     config.log_formatter = proc do |severity, datetime, progname, message|
       severity_with_bracket = "[#{severity}]"
       "#{severity_with_bracket.rjust(7)}[#{datetime.in_time_zone.to_s}]: #{progname} : #{message}\n"
