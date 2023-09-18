64a045e2c159b6b0855278a13adac82ff9a95ec8
shin4488 <efshinya48@gmail.com>
2023-09-19 01:07:32 +0900

    add: enabled google analytics

--- application/front/src/App.tsx
+++ application/front/src/App.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import './App.css';
 import DefaultLayout from '@/layouts/default/DefaultLayout';
 import FinancialStatementList from '@/pages/financialStatementList/FinancialStatementList';
+import FirebaseAnalytics from './plugins/firebase/analytics';
 
 export default class App extends React.Component {
+  componentDidMount(): void {
+    FirebaseAnalytics.getAnalytics();
+  }
+
   render(): React.ReactNode {
     return (
       <DefaultLayout>
