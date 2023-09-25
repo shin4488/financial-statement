c50b9d5aef6ce96674049ce10ce2b7040673c3b5
shin4488 <efshinya48@gmail.com>
2023-09-25 12:03:09 +0900

    change: order by for fiscal year end when fetching reports

--- application/server/app/models/security_report.rb
+++ application/server/app/models/security_report.rb
@@ -7,7 +7,7 @@ class SecurityReport < ApplicationRecord
       # 提出日のみのorderだと実行ごとに並び替え順番が異なり得るため、日時を値にもつupdated_atもorderに加えて、何回実行しても同じ並び順となるようにする
       SecurityReport.eager_load(:company)
         .where(accounting_standard: "japan_gaap")
-        .order(SecurityReport.arel_table[:filing_date].desc.nulls_last, updated_at: :desc)
+        .order(SecurityReport.arel_table[:filing_date].desc.nulls_last, fiscal_year_end_date: :desc, updated_at: :desc)
         .limit(limit)
         .offset(offset)
     end
