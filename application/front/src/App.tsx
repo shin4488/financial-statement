1571b5575bea19c5866cc42d66359c4a8d9d5413
shin4488 <efshinya48@gmail.com>
2024-06-11 23:56:44 +0900

    add: positive & negative theme color

--- application/front/src/App.tsx
+++ application/front/src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import './types/mui/styles.d';
 import './App.css';
 import DefaultLayout from '@/layouts/default/DefaultLayout';
 import FinancialStatementList from '@/pages/financialStatementList/FinancialStatementList';
 import FirebaseAnalytics from './plugins/firebase/analytics';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
 export default class App extends React.Component {
   componentDidMount(): void {
@@ -10,12 +12,32 @@ export default class App extends React.Component {
   }
 
   render(): React.ReactNode {
+    const theme = createTheme({
+      palette: {
+        positive: {
+          main: '#5A96E3',
+          light: '#5A96E3',
+          dark: '#A1C2F1',
+          contrastText: '#fff',
+        },
+        negative: {
+          main: '#E48586',
+          light: '#E48586',
+          dark: '#FF9EAA',
+          contrastText: '#fff',
+        },
+      },
+    });
+
     return (
-      <DefaultLayout>
-        <div className="App">
-          <FinancialStatementList />
-        </div>
-      </DefaultLayout>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <DefaultLayout>
+          <div className="App">
+            <FinancialStatementList />
+          </div>
+        </DefaultLayout>
+      </ThemeProvider>
     );
   }
 }
